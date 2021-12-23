6cba1646b678121ade3cfb0bd9cf6ba701fe912d
Robin Zhao <titanen.1987@hotmail.com>
2021-12-23 12:32:02 +0100

    test met pyfirmata

---
+++ pyfirmata/testfirmata.py
@@ -0,0 +1,18 @@
+import pyfirmata 
+import time
+
+
+
+
+led = 13
+
+try:
+    port = "COM7"
+    board = pyfirmata.Arduino(port)
+    print("connected")
+except:
+    print("failed to connect")
+
+while True:
+    board.digital[led].write(1)
+    
